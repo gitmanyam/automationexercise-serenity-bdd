a8a57c822823e6b422ae034c6de38221259a8603
gitmanyam <sunilmanyam2@gmail.com>
2026-07-01 05:11:14 +0100

    CheckoutActions

--- src/test/java/actions/CheckoutActions.java
+++ src/test/java/actions/CheckoutActions.java
@@ -6,7 +6,7 @@ import pages.CheckoutPage;
 import testdata.CheckoutData;
 
 public class CheckoutActions {
-
+    
     CheckoutPage checkoutPage;
 
     @Steps
@@ -19,18 +19,15 @@ public class CheckoutActions {
 
     @Step
     public void login(String email, String password) {
-
         loginActions.openApplication();
         loginActions.navigateToLoginPage();
         loginActions.verifyLoginPage();
         loginActions.enterCredentials(email, password);
         loginActions.clickLoginButton();
-
     }
 
     @Step
     public void addProduct(String product) {
-
         productActions.navigateToProductsPage();
         productActions.verifyProductsPage();
         productActions.searchProduct(product);
@@ -42,31 +39,26 @@ public class CheckoutActions {
 
     @Step
     public void proceedToCheckout() {
-
         checkoutPage.clickProceedToCheckout();
     }
 
     @Step
     public void verifyCheckoutPage() {
-
         checkoutPage.verifyCheckoutPage();
     }
-
+    
     @Step
     public void reviewAddress() {
-
         checkoutPage.checkDeliveryAddress(checkoutData);
     }
 
     @Step
     public void enterPaymentDetails() {
-
         checkoutPage.enterPaymentDetails(checkoutData);
     }
 
     @Step
     public void confirmOrder() {
-
         checkoutPage.clickPayAndConfirm();
     }
 
