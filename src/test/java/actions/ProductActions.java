3f677faf2a8d2c1070398613e5fd493cd152a0b8
gitmanyam <sunilmanyam2@gmail.com>
2026-07-01 05:15:22 +0100

    ProductActions

--- src/test/java/actions/ProductActions.java
+++ src/test/java/actions/ProductActions.java
@@ -9,50 +9,42 @@ public class ProductActions {
 
     @Step
     public void openApplication() {
-
         productPage.open();
     }
 
     @Step
     public void navigateToProductsPage() {
-
         productPage.acceptCookiesIfDisplayed();
         productPage.clickProducts();
     }
 
     @Step
     public void verifyProductsPage() {
-
         productPage.verifyProductsPage();
     }
 
     @Step
     public void searchProduct(String product) {
-
         productPage.searchProduct(product);
     }
 
     @Step
     public void verifySearchResults() {
-
         productPage.verifySearchResults();
     }
 
     @Step
     public void addProductToCart() {
-
         productPage.addProductToCart();
     }
 
     @Step
     public void navigateToCart() {
-
         productPage.viewCart();
     }
 
     @Step
     public void verifyProductInCart(String product) {
-
         productPage.verifyProductInCart(product);
     }
 
