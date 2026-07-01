0545a341dc8f50a16284e95d1e80a23b2e188b37
gitmanyam <sunilmanyam2@gmail.com>
2026-07-01 05:34:21 +0100

    ProductSteps

--- src/test/java/steps/ProductSteps.java
+++ src/test/java/steps/ProductSteps.java
@@ -11,43 +11,36 @@ public class ProductSteps {
 
     @Given("the user navigates to Homepage")
     public void openHomePage() {
-
         productActions.openApplication();
     }
 
     @When("the user clicks and navigates to the Products page")
     public void navigateToProducts() {
-
         productActions.navigateToProductsPage();
     }
 
     @Then("the user should be navigated to the Products page")
     public void verifyProductsPage() {
-
         productActions.verifyProductsPage();
     }
 
     @When("the user searches for the product {string}")
     public void searchProduct(String product) {
-
         productActions.searchProduct(product);
     }
 
     @Then("the searched product should be displayed")
     public void verifySearchResults() {
-
         productActions.verifySearchResults();
     }
 
     @When("the user adds the searched product to the cart")
     public void addProduct() {
-
         productActions.addProductToCart();
     }
 
     @And("the user navigates to the Cart page")
     public void viewCart() {
-
         productActions.navigateToCart();
     }
 
