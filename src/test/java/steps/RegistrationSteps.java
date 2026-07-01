df9e980597fac7f8d71319696177227ff5b82376
gitmanyam <sunilmanyam2@gmail.com>
2026-07-01 05:35:33 +0100

    RegistrationSteps

--- src/test/java/steps/RegistrationSteps.java
+++ src/test/java/steps/RegistrationSteps.java
@@ -14,19 +14,16 @@ public class RegistrationSteps {
 
     @Given("the user navigates to the Homepage")
     public void openHomePage() {
-
         registrationActions.openApplication();
     }
 
     @When("the user clicks and navigates to the Signup page")
     public void signupPage() {
-
         registrationActions.goToSignupPage();
     }
 
     @And("the user provides name {string} and email address {string}")
     public void enterUserDetails(String name, String email) {
-
         registrationActions.enterSignupDetails(name, email);
     }
 
@@ -37,37 +34,31 @@ public class RegistrationSteps {
 
     @Then("the user should be navigated to the Account Information page")
     public void verifyAccountInformationPage() {
-
         registrationActions.verifyAccountInformationPage();
     }
 
     @When("the user enters the required account information")
     public void enterAccountInformation() {
-
         registrationActions.enterAccountInformation();
     }
 
     @When("the user enters the address information")
     public void enterAddressInformation() {
-
         registrationActions.enterAddressInformation();
     }
 
     @And("the user submits the registration form")
     public void submitRegistration() {
-
         registrationActions.submitRegistration();
     }
 
     @Then("the user account should be created successfully")
     public void verifyAccountCreated() {
-
         registrationActions.verifyAccountCreated();
     }
 
     @Then("the user should see {string}")
     public void verifyValidationMessage(String expected) {
-
         registrationActions.verifyValidationMessage(expected);
     }
 }
